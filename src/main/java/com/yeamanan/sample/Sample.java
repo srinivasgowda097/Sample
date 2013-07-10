ee37e7db859ea83c17e54919d5fedd49e7cddf2e
yeamanan <yeamanan@gmail.com>
2013-07-10 15:48:06 +0200

    Review of the build file

--- src/main/java/com/yeamanan/sample/Sample.java
+++ src/main/java/com/yeamanan/sample/Sample.java
@@ -3,12 +3,16 @@ package com.yeamanan.sample;
 import java.io.IOException;
 import org.apache.log4j.Logger;
 
+/**
+ * Sample class.
+ * @author Yeam Anan <yeamanan@gmail.com>
+ */
 public class Sample {
 
-    static Logger logger = Logger.getLogger(Sample.class);
+    private static final Logger LOGGER = Logger.getLogger(Sample.class);
 
-    public static void main(String[] args) throws IOException {
-        logger.info("This is a sample project");
+    public static void main(final String[] args) throws IOException {
+        LOGGER.info("This is a sample project");
     }
 
 }
